af2041b64bdb44f386f847b61537821bc5b57a14
RishitM <72683716+RishitM@users.noreply.github.com>
2021-01-16 09:04:58 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,21 +1,26 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD1eXjaTOGsgMbpSdQXkmzHr15pNjW8Q-Q",
+  authDomain: "kwitter-a-chat-app.firebaseapp.com",
+  projectId: "kwitter-a-chat-app",
+  storageBucket: "kwitter-a-chat-app.appspot.com",
+  messagingSenderId: "699740989628",
+  appId: "1:699740989628:web:e94564a23786eca93a48b9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+document.getElementById("hello").innerHTML= ("Welcome "+ name + "!")
+function logout(){
+  window.location="index.html";
+}
 function add(){
       roomname=document.getElementById("room").value;
       localStorage.setItem("roomID", roomname);
       console.log("roomID= " + roomname);
+      firebase.database().ref("/").child(Room_Name).update({RoomName:roomname})
 }
-//ADD YOUR FIREBASE LINKS HERE
 
-  //Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyD1eXjaTOGsgMbpSdQXkmzHr15pNjW8Q-Q",
-    authDomain: "kwitter-a-chat-app.firebaseapp.com",
-    projectId: "kwitter-a-chat-app",
-    storageBucket: "kwitter-a-chat-app.appspot.com",
-    messagingSenderId: "699740989628",
-    appId: "1:699740989628:web:e94564a23786eca93a48b9"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
